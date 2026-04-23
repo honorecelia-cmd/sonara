7754cff958515c42ceed19ae181bd49ece6ce491
Célia Claire Honoré <celiaclairehonore@macbookpro.home>
2026-04-23 18:26:04 -0400

    fix sync - send order not full pool

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ server.on('upgrade', function(req, socket) {
       const msg = JSON.parse(frame.payload);
       if (msg.type === 'start' && rooms[code].host === playerId) {
         rooms[code].started = true;
-        broadcastAll(code, { type: 'game_start', theme: rooms[code].theme, pool: msg.pool });
+        broadcastAll(code, { type: 'game_start', theme: rooms[code].theme, order: msg.order });
       }
       if (msg.type === 'answer') {
         const pl = rooms[code].players.find(function(x){ return x.id === playerId; });
